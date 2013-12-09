d2cac617f69645c971820f6d154fe76a0be7b628
Colin Gagon <cgagon@turbosquid.com>
2013-12-09 14:38:02 -0600

    cleaner

--- lib/logstasher/log_subscriber.rb
+++ lib/logstasher/log_subscriber.rb
@@ -77,12 +77,9 @@ module LogStasher
 
     # Monkey patching to enable exception logging
     def extract_exception(payload)
-      if payload[:exception]
-        exception, message = payload[:exception]
-        { :status => 500, exception: exception, message: message, backtrace: $!.backtrace }
-      else
-        {}
-      end
+      return {} unless payload[:exception]
+      exception, message = payload[:exception]
+      { status: 500, exception: exception, message: message, backtrace: $!.backtrace }
     end
 
     def extract_custom_fields(payload)
